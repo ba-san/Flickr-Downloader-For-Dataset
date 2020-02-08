4698578b780e65d24b34af461d69cc45c0c082d2
OBA Daisuke <44015510+ba-san@users.noreply.github.com>
2020-02-09 00:05:43 +0900

    Add files via upload

--- json_cleaner.py
+++ json_cleaner.py
@@ -1,3 +1,4 @@
+import os
 import glob
 import json
 
@@ -7,7 +8,22 @@ directory = "cake"
 json_path = "./" + directory + "/info.json"
 
 json_open = open(json_path, 'r')
-d = json.load(json_open)
-print(d)
+json_load = json.load(json_open)
+
+json_list = json_load.keys()
+json_delete_list = []
+
+for json_file in json_list:
+	filename = glob.glob("./" + directory + "/" + json_file + ".*")
+	try:
+		filename[0]
+	except: # the case json's image is deleted in the folder
+		json_delete_list.append(json_file) # also delete json information
+
+for json_delete_file in json_delete_list:
+	json_load.pop(json_file) # also delete json information
+
+with open(json_path, 'w') as f:
+    json.dump(json_load, f, ensure_ascii=False)
 
 
